350524dd025dbaec9ed7ca7e0cd1ae8cc6f8b679
jonijermu <joni.heikkila@metropolia.fi>
2025-04-03 15:09:39 +0300

    removed todo comment

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -19,7 +19,6 @@ catRouter.route('/').get(getCat).post(upload.single('file'), createThumbnail, po
 
 catRouter.route('/:id').get(getCatById).put(putCat).delete(deleteCat);
 
-//TODO: Implement this route
 catRouter.route('/owner/:id').get(getCatByOwnerId);
 
 export default catRouter;
